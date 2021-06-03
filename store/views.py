f79183d1001e89e155e4273994072878babff1e3
anas4488 <anassayed63@gmail.com>
2021-06-03 21:18:50 +0530

    Seventh commit: veiws.py, settings.py

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 from .models import *
 
+def categories(request):
+    return(
+        {'categries':Category.objects.all()}
+    )
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html',{'products':products})

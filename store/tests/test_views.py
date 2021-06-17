c59279bfbef20b6352de8dfffc94460159d9ff21
anas4488 <anassayed63@gmail.com>
2021-06-17 14:17:39 +0530

    Eighteenth commit: store/test/test_views.py

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,10 +1,34 @@
 from unittest import skip
 
-from django.test import TestCase
 from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
 
-from store.models import *
+from store.models import Category, Product
+from store.views import product_all
 
-from django.test import Client
+
+@skip("demonstrating skipping")
+class TestSkip(TestCase):
+    def test_skip_exmaple(self):
+        pass
 
 
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
+                               slug='django-beginners', price='20.00', image='django')
+
+    def test_url_allowed_hosts(self):
+        """
+        Test allowed hosts
+        """
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
+        self.assertEqual(response.status_code, 200)

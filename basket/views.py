9c298c1932b2d1e79384c9657fbace7faae4f48a
anas4488 <anassayed63@gmail.com>
2021-06-25 14:37:38 +0530

    TwentySixth commit: templates/store/basket/summary.html, basket/basket.py, basket/urls.py, basket/views.py

--- basket/views.py
+++ basket/views.py
@@ -30,3 +30,16 @@ def basket_delete(request):
         basket.delete(product=product_id)
         response = JsonResponse({'Success':True})
         return response
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+
+        print(product_id)
+        print(product_qty)
+
+        response = JsonResponse({'Success':True})
+        return response
+        

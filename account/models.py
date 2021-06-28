0a2e268963f3e705582b970979e87538e28bc434
anas4488 <anassayed63@gmail.com>
2021-06-28 21:14:38 +0530

    TwentyEight commit: store/core/css/base.css, templates/store/base.html, core/settings.py, static/basket/, templates/store/products/category.html, templates/store/products/detail.html, templates/basket/summary.html, account/, accounts/models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email_address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, unique=True)
+    about = models.TextField(_('about'), max_length=500, unique=True)
+    #delivery_details
+    country = CountryField()

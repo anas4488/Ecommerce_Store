5200e86b9e8d366e206c6de1773cbf9357d33458
anas4488 <anassayed63@gmail.com>
2021-06-29 13:23:48 +0530

    TwentyNinth commit: account/models.py

--- account/models.py
+++ account/models.py
@@ -1,6 +1,19 @@
-from django.db import models
-
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.db import models
+from django_countries.fields import CountryField
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, user_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True')
+            
+
 
 class UserBase(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email_address'), unique=True)
@@ -9,3 +22,26 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     about = models.TextField(_('about'), max_length=500, unique=True)
     #delivery_details
     country = CountryField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    town_city = models.CharField(max_length=150, blank=True)
+    #User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+        def __str__(self):
+            return self.user_name
+            

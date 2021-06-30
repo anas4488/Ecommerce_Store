fc0dbe149fa328db229cd74d236f04d78e837de7
anas4488 <anassayed63@gmail.com>
2021-06-30 21:07:08 +0530

    TwentyNinth commit: account/forms.py, account/token.py, account/views.py,account/migrations, store/migrations, templates/account/

--- account/views.py
+++ account/views.py
@@ -14,4 +14,19 @@ def account_register(request):
             user.set_password(registerForm.cleaned_data['password'])
             user.is_active = False
             user.save()
-    
+            # Setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+
+            })
+            user.email_user(subject=subject, message=message)
+
+    else:
+        registerForm = RegistrationForm()
+
+    return render(request, 'account/register/register.html', {'form':registerForm})

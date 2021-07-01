886c0b62dd71ad976c622dc8939e4c2561765468
anas4488 <anassayed63@gmail.com>
2021-07-01 19:47:18 +0530

    Thirtieth commit: account/urls.py, account/views.py, core/urls.py, templates/account/registration/register.html, account/forms.py, core/settings.py, templates/account/registration/activation_invalid.html

--- account/views.py
+++ account/views.py
@@ -1,15 +1,22 @@
+from account.models import UserBase
+from django.conf.urls import url
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import RegistrationForm
 from .token import account_activation_token
 from django.template.loader import render_to_string
+from django.contrib.auth.decorators import login_required
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-
 from .forms import RegistrationForm
+
+@login_required
+def dashboard(request):
+    orders = user_orders(request)
+    return render(request, 'account/user/dashboard.html', {'section':'profile', 'orders':orders})
 
 def account_register(request):
     # if request.user.is_authenticated:
@@ -39,3 +46,15 @@ def account_register(request):
         registerForm = RegistrationForm()
 
     return render(request, 'account/registration/register.html', {'form':registerForm})
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
